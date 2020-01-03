ba159a4edb026a49209b91b04ce3c8e75035af14
Emil Gusgård <gusemil@hotmail.com>
2020-01-03 18:49:34 +0100

    Blood spatter fading

--- Assets/Scripts/BloodSplatter.cs
+++ Assets/Scripts/BloodSplatter.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BloodSplatter : MonoBehaviour
 {
+    private float bloodSplatterTimeToDestroy = 10f;
+    private SpriteRenderer sr;
+    private float bloodTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        bloodTimer = bloodSplatterTimeToDestroy;
+        //sr.color = new Color(1f, 1f, 1f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bloodTimer -= Time.deltaTime;
+        sr.color = new Color(1f, 1f, 1f, bloodTimer / 10);
+        Destroy(gameObject, bloodSplatterTimeToDestroy);
     }
 }
